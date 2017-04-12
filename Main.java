ece16ebfbe8179d754977f80e656ed2bab457526
Leandro <carlosl.cp@hotmail.com>
2017-04-12 01:10:49 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package DelayQueue;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.DelayQueue;
+
+public class Main {
+	public static void main(String[] args) {
+		BlockingQueue<Produto> c = new DelayQueue<Produto>();
+		Produtor p1 = new Produtor(c, 1);
+		Consumidor c1 = new Consumidor(c, 1);
+		
+		p1.start();
+        c1.start();
+	}
+}
